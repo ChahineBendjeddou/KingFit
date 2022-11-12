c279f7a7eeebc7b8962b192047f445be2d1379bb
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2022-11-12 15:59:01 +0100

    Testing

--- app.js
+++ app.js
@@ -1,66 +1,65 @@
-const express = require('express')
-const session = require('express-session')
-const flash = require('connect-flash')
-const app = express()
-const path = require('path');
-const DataBase = require('./database')
-const port = 3000 // default
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const User = require('./models/user');
+const express = require("express");
+const session = require("express-session");
+const flash = require("connect-flash");
+const app = express();
+const path = require("path");
+const DataBase = require("./database");
+const port = 3000; // default
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const ExpressError = require('./utils/ExpressError')
-// routes 
-const userRoutes = require('./routes/users')
+const ExpressError = require("./utils/ExpressError");
+// routes
+const userRoutes = require("./routes/users");
 
-
+//testing
 //setting up our app
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 const sessionConfig = {
-    secret: 'this is a secret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig))
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+  secret: "this is a secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
-    next()
-})
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
+app.use("/", userRoutes);
 
-app.use('/', userRoutes)
+app.get("/", (req, res, next) => {
+  const pageTitle = "KingFit";
+  res.status(200).render("home", { pageTitle });
+});
 
-app.get('/', (req, res, next) => {
-    const pageTitle = 'KingFit'
-    res.status(200).render('home', { pageTitle })
-})
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
-})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
 
-app.listen(port, () => console.log(`Serving on port ${port}`))
+app.listen(port, () => console.log(`Serving on port ${port}`));
--- database.js
+++ database.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 class Database {
+  //testing
+  constructor() {
+    this.connect();
+  }
 
-    constructor() {
-        this.connect()
-    }
-
-    connect() {
-        mongoose.connect('mongodb://localhost:27017/KingGym')
-            .then(() => console.log('DataBase connection successful'))
-            .catch(err => console.log('Error connecting to database ' + err.message))
-    }
+  connect() {
+    mongoose
+      .connect("mongodb://localhost:27017/KingGym")
+      .then(() => console.log("DataBase connection successful"))
+      .catch((err) =>
+        console.log("Error connecting to database " + err.message)
+      );
+  }
 }
 
-module.exports = new Database()
+module.exports = new Database();
